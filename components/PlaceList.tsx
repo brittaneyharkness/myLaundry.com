7b6855c32ccff0092d17998daca854c305bdc263
brittaneyharkness <brittaney.harkness@cookcountyil.gov>
2025-03-11 23:25:07 -0500

    transition to cards

--- components/PlaceList.tsx
+++ components/PlaceList.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import places from '../data/listing.json'
 import regions from '../data/regions.json'
 import { useSearchParams } from 'next/navigation';
-import { CalciteCard, CalciteTile, CalciteTileGroup } from '@esri/calcite-components-react';
+import { CalciteCard, CalciteCardGroup, CalciteTile, CalciteTileGroup } from '@esri/calcite-components-react';
 
 import "@esri/calcite-components/components/calcite-card"
-import "@esri/calcite-components/components/calcite-tile"
+import "@esri/calcite-components/components/calcite-card-group"
 
 function PlaceList() {
 
@@ -18,15 +18,22 @@ function PlaceList() {
 
       
   return (
-    <div className='flex gap-3 mt-5'>
+    <CalciteCardGroup className='mt-10 flex justify-evenly'>
         {
           places.map(place => {
             if(city && state){
               if(place.city === city && place.state === state_abbr[0]){
                 return(
-                  <CalciteCard label={place.name}>
-                    <div>
-                      
+                  <CalciteCard 
+                    key={place.id}
+                    label={place.name}
+                    className='w-[20%] p-2'
+                    >
+                    <div slot="heading" className='pb-5'>
+                    {place.name}    
+                    </div>
+                    <div slot="description">
+                    {place.summary}    
                     </div>
                   </CalciteCard> 
 
@@ -36,8 +43,16 @@ function PlaceList() {
             if(!city && state){
               if(place.state === state_abbr[0]){
                 return(
-                  <CalciteCard label={place.name}>
-                   
+                  <CalciteCard 
+                  key={place.id}
+                  label={place.name}
+                  className='w-[20%] p-2'>
+                   <div slot="heading" className='pb-5'>
+                    {place.name}    
+                    </div> 
+                    <div slot="description">
+                    {place.summary}    
+                    </div>
                   </CalciteCard> 
                 )
               }
@@ -46,7 +61,7 @@ function PlaceList() {
             
           })
         }
-    </div>
+    </CalciteCardGroup>
   )
 }
 
